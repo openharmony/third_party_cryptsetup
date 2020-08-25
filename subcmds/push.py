a1c0ee8e62ccb8dfc399458e24ed192fa2f7bad4
江畅 <631866165@qq.com>
2020-08-25 16:48:51 +0800

    compatible python2

--- subcmds/push.py
+++ subcmds/push.py
@@ -13,11 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from __future__ import print_function
 import copy
 import re
 import sys
-
 from command import InteractiveCommand
 from editor import Editor
 from error import UploadError, GitError, PullRequestError
